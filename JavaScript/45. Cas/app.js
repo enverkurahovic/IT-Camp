aab3da7a7a0df2ba420647d9835613e11c2f5b52
enverkurahovic <ekisha818@gmail.com>
2024-03-17 21:19:24 +0100

    priprema

---
+++ JavaScript/45. Cas/app.js
@@ -0,0 +1,39 @@
+// test prep
+
+// test has 2 parts
+
+// First part
+// 10 teoriskih pitanja iz oblasti navedenih u 44-tom casu
+// najvise pitanja iz simple js  i 2-3 pitanja iz advenced i medium pitanja
+
+// select a primitive js type? i onda abcd, odgovori.
+// what are coplext types in js? abcd
+// what are scopes of const, let, and var
+// what are diffreces between let and const...
+// what is the result of the code console.log("yes" || "no")
+// what is the result of the code (if){true(console.log(true))}
+// what are the loop used in js
+// what is an object in js complex str used in js
+// how do we dinamically acces values inside of the objects
+// what are the property accessor for objects(dot and [])
+// which are the array function
+// what does this code do? [].map(() => {})
+// ADVANCED only definiton of concepts
+
+// Part 2
+
+// Parctical part.
+// 40 minuta za ovaj deo. Po potrebi i vise.
+// Imacete pristup internetu i svemu sto smo radili do sada.
+// chatgpt ne dolazi u obzir.
+// 2 pitanja.
+// Morate napraviti repositori na svom github nalogu gde ce te pushati kod
+// link repozitoriju ce te ostaviti na google formu za ovaj deo
+
+// Prvo Pitanje:
+// Bice vise algoritamskog tipa gde imate neku obradu podatka i vi trebate nesto sa tim podacima 5,6,7 cas
+// Drugo Pitanje:
+// Treba fetchati nesto sa nekog apija i onda prikazati na stanici
+// moguca dodatna komplikacija gde ce te morati i na neki event fetchati podatke
+
+// Imamo primere od 9 casa pa nadalje. A navise na casu 15.
