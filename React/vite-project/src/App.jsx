6a09fce86467f17bd4307b1e630aecc41a1bfee1
enverkurahovic <ekisha818@gmail.com>
2024-05-24 14:50:40 +0200

    idk

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const arr = [];
+  for (let i = 0; i < 10; i++) {
+    arr.push(i);
+  }
 
   return (
     <>
@@ -18,8 +23,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <button onClick={() => setCount((count) => count - 1)}>
+          dec {count}
+        </button>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          cou {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -29,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
